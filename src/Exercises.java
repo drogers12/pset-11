b44f2fe6fe6488769ca8b207bb934e7976669c5e
drogers12 <54987779+drogers12@users.noreply.github.com>
2020-03-22 21:14:14 -0400

    findMe Both Working

--- src/Exercises.java
+++ src/Exercises.java
@@ -22,7 +22,7 @@ public class Exercises {
       }
 
       for (int i = 0; i < list.size(); i++) {
-          if (list.get(i) = target) {
+          if (list.get(i) == target) {
               return i;
           }
       }
