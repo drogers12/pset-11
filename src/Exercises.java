a8fe187be382e400d70c46ac06eaa4a24ff6906b
drogers12 <54987779+drogers12@users.noreply.github.com>
2020-03-22 21:29:47 -0400

    !ascending insertion working

--- src/Exercises.java
+++ src/Exercises.java
@@ -151,6 +151,21 @@ public class Exercises {
                   }
               }
           }
+      } else if (!ascending) {
+          for (int i = 1; i < list.size(); i++) {
+              int key = list.get(i);
+              for (int j = i - 1; j >= 0; j--) {
+                  if (key > list.get(j)) {
+                      list.set(j + 1,list.get(j));
+                      if (j == 0) {
+                          list.set(0, key);
+                      }
+                  } else {
+                      list.set(j + 1, key);
+                      break;
+                  }
+              }
+          }
       }
       return list;
   }
